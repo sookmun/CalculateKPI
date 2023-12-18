484854dcc80085b1a24cfe469de2ea32f1ba5269
Tan Sook Mun <stan0111@student.monash.edu>
2023-12-18 22:50:32 +0800

    clean up code

--- CalculateKPI.py
+++ CalculateKPI.py
@@ -4,14 +4,11 @@ def calculate():
     dataframe1 = pd.read_excel('KPI.xlsx')
     dataframe1 = pd.DataFrame(dataframe1)
 
-    f =dataframe1.mean(axis='columns', numeric_only=True)
-    print(f)
-    col = ['Q1', 'Q2','Q3','Q4']
-    dataframe1['total'] = dataframe1[col].sum(axis=1)
-    dataframe1['average'] = dataframe1[col].mean(numeric_only=True, axis=1)
+    col = ['Q1', 'Q2', 'Q3', 'Q4']
+    dataframe1['Total'] = dataframe1[col].sum(axis=1)
+    dataframe1['Average'] = dataframe1[col].mean(axis=1)
     print(dataframe1)
-    dataframe1.to_excel('output.xlsx', index=False)
-    return None
+    dataframe1.to_excel('calculatedKPI.xlsx', index=False)
 
 
 
